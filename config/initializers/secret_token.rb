42fc64c3f42bd1ab2386407ada73d280e82bf1b3
Brian Bommarito <brian@bbommarito.me>
2011-04-26 06:38:14 -0500

    First.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osfl::Application.config.secret_token = 'bb994e8a5eb1da5911226cd6c737f8735730951bd34f224993d2ae9449f22e412970b1aa38c7c1d3a94b9cfd8d9233a2d758e414596eba0b452ed0aba68216c0'
